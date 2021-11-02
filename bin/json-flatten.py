888151acdfc140e9574324d77fd61541ee430387
Jud Dagnall <github@dagnall.net>
2021-11-01 21:37:59 -0700

    add som arg handling
    * --file-prefix
    * --path-prefix

--- bin/json-flatten.py
+++ bin/json-flatten.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from pathlib import Path
 
+import argparse
 import fileinput
 import json
 import logging
@@ -26,11 +28,28 @@ class Flattener:
             out[key] = data
         return out
 
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('-f', '--file-prefix', action='store_true',
+            help='use the filename as a prefix')
+    p.add_argument('-p', '--path-prefix', action='store_true',
+            help='use the path as a prefix')
+    p.add_argument('input', nargs='*', help='input files, or stdin')
+    return p.parse_args()
+
 def run():
+    opts = parse_args()
     flattener = Flattener()
-    for line in fileinput.input():
+    for line in fileinput.input(opts.input):
         data = json.loads(line.strip())
-        print(json.dumps(flattener.flatten(data, )))
+        prefix=None
+        if opts.file_prefix:
+            prefix = Path(fileinput.filename()).stem()
+        elif opts.path_prefix:
+            p = Path(fileinput.filename())
+            prefix = p.parent / p.stem
+        flattened = flattener.flatten(data, prefix=[str(prefix)])
+        print(json.dumps(flattened))
 
 if __name__ == '__main__':
     run()
