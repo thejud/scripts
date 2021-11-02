0a1991122a509d9ca528248f43619bb5217dd414
Jud Dagnall <github@dagnall.net>
2021-11-01 21:32:54 -0700

    bulk replace based on lookup table

---
+++ bin/lookupfilter.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+NAME:
+
+  lookupfilter: lookup and replace multiple kv pairs
+
+DESCRIPTION:
+
+    Lookup and replace multiple strings in the input
+    given a set of pattern -> replacement pairs
+    in a tab-separated lookup file.
+
+
+AUTHOR:
+
+  Jud Dagnall <github@dagnall.net>
+
+EXAMPLES:
+
+    # common usage: replace all ip addresses with their hostnames
+    cat log | lookupfilter --lookup ip2host.tsv 
+
+"""
+
+from __future__ import print_function
+
+import argparse
+import fileinput
+import json
+import logging
+import sys
+
+TIMESTAMP_FORMAT='%(asctime)s %(levelname)s - %(message)s'
+
+def parse_args(args=None):
+    desc=""
+    p = argparse.ArgumentParser(description=desc)
+    #p.add_argument('', help="default: %(default)s", default='')
+    p.add_argument('-d', '--delimiter', 
+            default="\t",
+            help="delimiter for lookup file. Default: %(default)s")
+    p.add_argument('-l', '--lookup', 
+            required=True,
+            help="tab-separated lookup table k->v")
+    p.add_argument('input', nargs='*', 
+            help='input files to filter. Default: stdin')
+  
+    # accept arguments as a param, so we
+    # can import and run this module with a commandline-like
+    # syntax.
+    if args is None: 
+        args = sys.argv[1:]
+    return p.parse_args(args)
+
+def load_lookups(opts):
+    lookups = {}
+    delimiter = opts.delimiter or "\t"
+    logging.info(f"reading lookups: {opts.lookup}")
+    with open(opts.lookup, 'r') as fh:
+        for line in fh:
+            k, v = line.rstrip().split(delimiter, 2)
+            lookups[k] = v
+    logging.debug(f"lookup count: {len(lookups.keys())}")
+    return lookups
+
+def replace_lines(opts, lookups):
+    for line in fileinput.input(opts.input):
+        for k,v in lookups.items():
+            if k in line:
+                line = line.replace(k, v)
+        yield line
+
+def run(opts):
+    logging.debug("starting")
+    lookups = load_lookups(opts)
+    for line in replace_lines(opts, lookups):
+        print(line, end="")
+
+if __name__ == '__main__':
+    opts = parse_args(sys.argv[1:])
+    logging.basicConfig(level=logging.DEBUG,format=TIMESTAMP_FORMAT)
+    run(opts)
