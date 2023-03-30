0d16c9d8524b28a647082058e73a5a019aa84e9a
Jud Dagnall <github@dagnall.net>
2023-03-30 14:15:51 -0700

    Support compressed json output with -c

--- bin/yaml2json.py
+++ bin/yaml2json.py
@@ -7,7 +7,16 @@ from __future__ import print_function
 import json
 import fileinput
 import yaml
+import argparse
+import sys
 
-yaml_data = "".join([line for line in fileinput.input()])
-print(json.dumps(yaml.safe_load(yaml_data), sort_keys=True, indent=2))
+parser = argparse.ArgumentParser()
+parser.add_argument('--compact', '-c', action='store_true', help='enable compact mode')
+parser.add_argument('files', nargs='*')
+args = parser.parse_args()
+
+indent = None if args.compact else 2
+
+yaml_data = "".join([line for line in fileinput.input(args.files)])
+print(json.dumps(yaml.safe_load(yaml_data), sort_keys=True, indent=indent))
 
