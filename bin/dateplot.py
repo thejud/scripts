ed012062708c15628548aceb584951f3c97edc3b
Jud Dagnall <github@dagnall.net>
2024-10-19 23:39:40 -0700

    reformat and some code cleanup

--- bin/dateplot.py
+++ bin/dateplot.py
@@ -6,7 +6,7 @@ NAME:
 
 DESCRIPTION:
 
-    Every wanted to quickly visualize some log output from the command line?
+    Ever wanted to quickly visualize some log output from the command line?
     I often grab some data out of a logfile, and then want to visualize it, e.g.
     What is the hourly rate of this error message.
     Input is two columns. Unless --basic is specified, the first column is a
@@ -74,7 +74,7 @@ There are a lot of options so that it can be quickly used for a variety of stuff
 
 """
 
-from __future__ import print_function
+from io import StringIO
 
 import argparse
 import fileinput
@@ -82,118 +82,150 @@ import logging
 import os
 import sys
 
-from pandas import Series, DataFrame
-import pandas
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
-
-pd = pandas
-plt = matplotlib.pyplot
-
-cf0 = '{:,.0f}'.format
-cf2 = '{:,.2f}'.format
+cf0 = "{:,.0f}".format
+cf2 = "{:,.2f}".format
 
 
-TIMESTAMP_FORMAT='%(asctime)s %(levelname)s - %(message)s'
+TIMESTAMP_FORMAT = "%(asctime)s %(levelname)s - %(message)s"
+
 
 def parse_args(args=None):
-    desc=""
+    desc = ""
     p = argparse.ArgumentParser(description=desc)
-    p.add_argument('--csv', '-c', action='store_true',
-        help='input is CSV. Otherwise, whitespace is assumed'
-        )
-    p.add_argument('--tsv', '--tabs','-t', action='store_true',
-        help='input is tab separated. Otherwise, whitespace is assumed'
-        )
-    p.add_argument('--month', '-m', action='store_true',
-        help='output date with month precision')
-    p.add_argument('--day', '-d', action='store_true',
-        help='output date with day precision')
-    p.add_argument('--hour', '-H', action='store_true',
-        help='output date with hour precision. This is the default')
-    p.add_argument('--minute', '-M', action='store_true',
-        help='output date with minute precision.')
-    p.add_argument('--output-date-format', '-D',
-        help='strftime string for date output, e.g. "%%Y-%%m-%%dT%%H"')
-    p.add_argument('--rotation', '-r', type=float,
-        help='rotation angle for date output. Default=%(default)s',
-        default=30,)
-    p.add_argument('--basic', '-b', action='store_true',
-            help='basic plot. First column is not a datetime')
+    p.add_argument(
+        "--csv",
+        "-c",
+        action="store_true",
+        help="input is CSV. Otherwise, whitespace is assumed",
+    )
+    p.add_argument(
+        "--tsv",
+        "--tabs",
+        "-t",
+        action="store_true",
+        help="input is tab separated. Otherwise, whitespace is assumed",
+    )
+    p.add_argument(
+        "--month", "-m", action="store_true", help="output date with month precision"
+    )
+    p.add_argument(
+        "--day", "-d", action="store_true", help="output date with day precision"
+    )
+    p.add_argument(
+        "--hour",
+        "-H",
+        action="store_true",
+        help="output date with hour precision. This is the default",
+    )
+    p.add_argument(
+        "--minute", "-M", action="store_true", help="output date with minute precision."
+    )
+    p.add_argument(
+        "--output-date-format",
+        "-D",
+        help='strftime string for date output, e.g. "%%Y-%%m-%%dT%%H"',
+    )
+    p.add_argument(
+        "--rotation",
+        "-r",
+        type=float,
+        help="rotation angle for date output. Default=%(default)s",
+        default=30,
+    )
+    p.add_argument(
+        "--basic",
+        "-b",
+        action="store_true",
+        help="basic plot. First column is not a datetime",
+    )
 
-    p.add_argument('--outfile', '-o', 
-        help='output file. default=%(default)s',
-        default='/tmp/dateplot.png')
-    p.add_argument('--symbol', '-s', 
-            help='alternate symbol, e.g. "*" for chart')
-    p.add_argument('--title', '-T', 
-            help="title for the chart")
+    p.add_argument(
+        "--outfile",
+        "-o",
+        help="output file. default=%(default)s",
+        default="/tmp/dateplot.png",
+    )
+    p.add_argument("--symbol", "-s", help='alternate symbol, e.g. "*" for chart')
+    p.add_argument("--title", "-T", help="title for the chart")
 
-    p.add_argument('-x', type=float, help='chart width (inches)')
-    p.add_argument('-y', type=float, help='chart height (inches)')
-    p.add_argument('--large', '-L', action='store_true',
-            help='large output dimensions, 10x12',)
+    p.add_argument("-x", type=float, help="chart width (inches)")
+    p.add_argument("-y", type=float, help="chart height (inches)")
+    p.add_argument(
+        "--large",
+        "-L",
+        action="store_true",
+        help="large output dimensions, 10x12",
+    )
 
-    p.add_argument('--points', '-p', 
+    p.add_argument(
+        "--points",
+        "-p",
         help='matplotlib point format string., e.g. "." or "xb-" default=%s(default)',
-        default='-')
+        default="-",
+    )
 
-    p.add_argument('input', nargs='*',
-        help='zero or more input files. Otherwise read from STDIN',)
-  
+    p.add_argument(
+        "input",
+        nargs="*",
+        help="zero or more input files. Otherwise read from STDIN",
+    )
+
     # accept arguments as a param, so we
     # can import and run this module with a commandline-like
     # syntax.
-    if args is None: 
+    if args is None:
         args = sys.argv[1:]
     return p.parse_args(args)
 
-def get_data(opts):
+
+def get_data(opts: argparse.Namespace) -> pd.DataFrame:
     args = {
-    'names':['datetime', 'val'],
+        "names": ["datetime", "val"],
+        "sep": r"\s+",
     }
     if not opts.basic:
-        args['parse_dates']=['datetime']
+        args["parse_dates"] = ["datetime"]
 
     if opts.tsv:
-         args['sep'] = '\t'
-        
+        args["sep"] = "\t"
+
     elif opts.csv:
-         args['sep'] = ','
-    else:
-        args['sep'] = r"\s+"
-    
-    indata = pd.compat.StringIO("".join([s for s in fileinput.input(opts.input)]))
+        args["sep"] = ","
+
+    indata = StringIO("".join([s for s in fileinput.input(opts.input)]))
     df = pd.read_table(indata, **args)
 
     logging.debug(df.head())
     logging.debug("dtype: %s", df.dtypes)
 
-    #df = df.sort_values('datetime', ascending=True)
+    # df = df.sort_values('datetime', ascending=True)
     return df
+
 
 def get_date_output(opts):
     if opts.day:
-        fmt = '%Y-%m-%d'
+        fmt = "%Y-%m-%d"
     elif opts.month:
-        fmt = '%Y-%m'
+        fmt = "%Y-%m"
     elif opts.hour:
-        fmt = '%Y-%m-%d %H'
+        fmt = "%Y-%m-%d %H"
     elif opts.minute:
-        fmt = '%Y-%m-%d %H:%M'
+        fmt = "%Y-%m-%d %H:%M"
     elif opts.output_date_format:
         fmt = opts.output_date_format
     else:
-        fmt = '%Y-%m-%d %H'
+        fmt = "%Y-%m-%d %H"
     return fmt
 
 
 def plot(df, opts):
-
-    dims = [6,4]
+    dims = [6, 4]
     if opts.large:
-        dims = [20,10]
+        dims = [20, 10]
     if opts.x:
         dims[0] = opts.x
     if opts.y:
@@ -201,11 +233,12 @@ def plot(df, opts):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(df['datetime'], df['val'], opts.points)
+    ax.plot(df["datetime"], df["val"], opts.points)
     date_format = get_date_output(opts)
 
     if not opts.basic:
         import matplotlib.dates as mdates
+
         ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
         # rotate and align the tick labels so they look better
         fig.autofmt_xdate(rotation=opts.rotation, bottom=0.3)
@@ -213,19 +246,21 @@ def plot(df, opts):
     if opts.title:
         plt.title(opts.title)
 
-    fig.set_size_inches(*dims)
+    fig.set_size_inches(w=dims[0], h=dims[1])
     plt.savefig(opts.outfile)
+
 
 def run(opts):
     logging.debug("starting")
     df = get_data(opts)
     plot(df, opts)
 
-    cmd = ['open', opts.outfile]
+    cmd = ["open", opts.outfile]
     logging.info(cmd)
     os.execvp(cmd[0], cmd)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     opts = parse_args(sys.argv[1:])
-    logging.basicConfig(level=logging.DEBUG,format=TIMESTAMP_FORMAT)
+    logging.basicConfig(level=logging.DEBUG, format=TIMESTAMP_FORMAT)
     run(opts)
