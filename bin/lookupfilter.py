637dcc7bf456e774095e1127880fe28134986d67
Jud Dagnall <github@dagnall.net>
2024-10-28 02:14:34 -0700

    reformat and typing

--- bin/lookupfilter.py
+++ bin/lookupfilter.py
@@ -18,7 +18,7 @@ AUTHOR:
 EXAMPLES:
 
     # common usage: replace all ip addresses with their hostnames
-    cat log | lookupfilter --lookup ip2host.tsv 
+    cat log | lookupfilter --lookup ip2host.tsv
 
 """
 
@@ -26,57 +26,63 @@ from __future__ import print_function
 
 import argparse
 import fileinput
-import json
 import logging
 import sys
 
-TIMESTAMP_FORMAT='%(asctime)s %(levelname)s - %(message)s'
+TIMESTAMP_FORMAT = "%(asctime)s %(levelname)s - %(message)s"
 
-def parse_args(args=None):
-    desc=""
+
+def parse_args(args=None) -> argparse.Namespace:
+    desc = "lookup and replace multiple kv pairs"
     p = argparse.ArgumentParser(description=desc)
-    #p.add_argument('', help="default: %(default)s", default='')
-    p.add_argument('-d', '--delimiter', 
-            default="\t",
-            help="delimiter for lookup file. Default: %(default)s")
-    p.add_argument('-l', '--lookup', 
-            required=True,
-            help="tab-separated lookup table k->v")
-    p.add_argument('input', nargs='*', 
-            help='input files to filter. Default: stdin')
-  
+    # p.add_argument('', help="default: %(default)s", default='')
+    p.add_argument(
+        "-d",
+        "--delimiter",
+        default="\t",
+        help="delimiter for lookup file. Default: %(default)s",
+    )
+    p.add_argument(
+        "-l", "--lookup", required=True, help="tab-separated lookup table k->v"
+    )
+    p.add_argument("input", nargs="*", help="input files to filter. Default: stdin")
+
     # accept arguments as a param, so we
     # can import and run this module with a commandline-like
     # syntax.
-    if args is None: 
+    if args is None:
         args = sys.argv[1:]
     return p.parse_args(args)
 
-def load_lookups(opts):
+
+def load_lookups(opts: argparse.Namespace) -> dict[str, str]:
     lookups = {}
     delimiter = opts.delimiter or "\t"
     logging.info(f"reading lookups: {opts.lookup}")
-    with open(opts.lookup, 'r') as fh:
+    with open(opts.lookup, "r") as fh:
         for line in fh:
             k, v = line.rstrip().split(delimiter, 2)
             lookups[k] = v
     logging.debug(f"lookup count: {len(lookups.keys())}")
     return lookups
 
-def replace_lines(opts, lookups):
+
+def replace_lines(opts: argparse.Namespace, lookups: dict[str, str]):
     for line in fileinput.input(opts.input):
-        for k,v in lookups.items():
+        for k, v in lookups.items():
             if k in line:
                 line = line.replace(k, v)
         yield line
 
-def run(opts):
+
+def run(opts: argparse.Namespace):
     logging.debug("starting")
     lookups = load_lookups(opts)
     for line in replace_lines(opts, lookups):
         print(line, end="")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     opts = parse_args(sys.argv[1:])
-    logging.basicConfig(level=logging.DEBUG,format=TIMESTAMP_FORMAT)
+    logging.basicConfig(level=logging.DEBUG, format=TIMESTAMP_FORMAT)
     run(opts)
